1a64c5d462b3f0df8e5cb380b4c100d169402c49
Justyna Mackoś <j.mackos@wp.pl>
2017-11-17 13:13:36 +0200

    Tank Track Class added

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
