e213372d7e66d6f9dab1c45c48f65627e8a446e6
Justyna Mackoś <j.mackos@wp.pl>
2017-11-17 13:41:35 +0200

    Tank Track Throttle added

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -1,7 +1,5 @@
-
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
 #include "TankTrack.generated.h"
 
@@ -11,7 +9,9 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+	UFUNCTION(BlueprintCallable, Category=Input)
+	void SetThrottle(float Throttle);
+
 	
 };
